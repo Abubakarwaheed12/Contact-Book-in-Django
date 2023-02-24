9e043205aedc2a8e78e91b544456b37eadd54a96
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-02-25 01:25:52 +0500

    -

--- app/views.py
+++ app/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-
+from app.models import Contact
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    allcontact=Contact.objects.all()
+    print(allcontact)
+    if request.GET.get('name'):
+        allcontact=Contact.objects.filter(name__contains=request.GET.get('name'))
+    allcontact
+    context={
+        'contacts':allcontact,
+    }
+    return render(request, 'index.html' , context)
 
 
 def edit(request):

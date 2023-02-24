dac5bc2b6a75449aad016f01fa2dcab45f26b244
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-02-25 01:12:37 +0500

    -

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    name=models.CharField(max_length=200)
+    relation_ship=models.CharField(max_length=300)
+    email=models.EmailField(max_length=255)
+    phone=models.CharField(max_length=200)
+    address=models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
